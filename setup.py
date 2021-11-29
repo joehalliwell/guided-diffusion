9af98758a3e8e5d5c2821a4422e2abd8d3627e03
Joe Halliwell <joe.halliwell@gmail.com>
2021-11-29 18:02:05 +0000

    Add missing mpi4py dep

--- setup.py
+++ setup.py
@@ -3,5 +3,5 @@ from setuptools import setup
 setup(
     name="guided-diffusion",
     packages=["guided_diffusion"],
-    install_requires=["blobfile>=1.0.5", "torch", "tqdm"],
+    install_requires=["blobfile>=1.0.5", "torch", "tqdm", "mpi4py"],
 )
